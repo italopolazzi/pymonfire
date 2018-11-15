87ec85a2152ceffda5a96c7a673b9a4e8bb3fd8a
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2018-11-15 01:32:25 -0200

    Sync working - beta 0.0.1

--- firebase.py
+++ firebase.py
@@ -3,12 +3,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+
 import os
 dirname = os.path.dirname(__file__)
 
 class MyFirebase:
     def __init__(self, collection):
-        self.cred = credentials.Certificate(os.path.join(dirname, 'keys.json'))
+        self.cred = credentials.Certificate(os.path.join(dirname, 'tcc_firebase_keys.json'))
         firebase_admin.initialize_app(self.cred)
         self.db = firestore.client()
         self.setCollection(collection)
@@ -25,5 +26,11 @@ class MyFirebase:
     def getWhereAnd(self, k1, o1, v1, k2, o2, v2):
         return self.coll.where(k1, o1, v1).where(k2, o2, v2).get()
 
+    def createOne(self, data):
+        return self.coll.add(data)
+
     def updateOne(self, id, data):
         return self.coll.document(id).update(data)
+
+    def setOne(self, id, data):
+        return self.coll.document(id).set(data)
